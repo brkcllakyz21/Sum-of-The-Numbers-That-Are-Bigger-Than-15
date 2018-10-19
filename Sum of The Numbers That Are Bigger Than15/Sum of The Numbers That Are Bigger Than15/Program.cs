f4eb50282f2a29308d132c4a2d3b573ee6a90c83
brkcllakyz21 <37548000+brkcllakyz21@users.noreply.github.com>
2018-10-20 00:40:06 +0300

    Add files via upload

---
+++ Sum of The Numbers That Are Bigger Than15/Sum of The Numbers That Are Bigger Than15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_The_Numbers_That_Are_Bigger_Than15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayaç = 0, toplam = 0, sayı = 0;
+
+            while (sayaç < 20)
+            {
+                Console.Write("Sayıyı Giriniz = ");
+                sayı = Convert.ToInt32(Console.ReadLine());
+                if (sayı > 15)
+                {
+                    toplam += sayı;
+                }
+                sayaç += 1;
+            }
+
+            Console.WriteLine("Girdiğiniz sayılardan 15'den büyük olanlarının toplamı = " + toplam);
+
+        }
+    }
+}
